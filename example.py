Fixed formula

example.py
@@ -1,42 +1,39 @@
 
 from pytorch_interp import RegularGridInterpolator as my_rgi
 from scipy.interpolate import RegularGridInterpolator as scipy_rgi
-
 import torch
 import numpy as np
 import matplotlib.pyplot as plt
-torch.cuda.empty_cache()
 
-device="cuda"
+device="cpu"
+torch.set_num_threads(8)
 
-M1  = 1000
-M2  = 500
-N   = 2**15
-x1  = -2
-x2  = 12
-y1  = -1.2
-y2  = 6
+M1  = 2**5
+M2  = 2**5
+N   = 2**10
+x1  = -4.23
+x2  = 12.6
+y1  = -2.3
+y2  = 2.2
 x   = torch.linspace(x1,x2,M1).to(device)
 y   = torch.linspace(y1,y2,M2).to(device)
 xpt = (x2-x1)*torch.rand(N)+x1
-xpt=xpt.to(device)
+xpt = xpt.to(device)
 ypt = (y2-y1)*torch.rand(N)+y1
-ypt=ypt.to(device)
+ypt = ypt.to(device)
 X, Y = torch.meshgrid(x, y, indexing="ij")
 
 F=torch.sin(X)*torch.sin(Y)
 
 # scipy implementation
-interp_spline = scipy_rgi((x.cpu().numpy(), y.cpu().numpy()), F.cpu().numpy()) 
-G1 = interp_spline(np.array([xpt.cpu().numpy(), ypt.cpu().numpy()]).T)
+interp1 = scipy_rgi((x.cpu().numpy(), y.cpu().numpy()), F.cpu().numpy(),bounds_error=False, fill_value=0.0) 
+G1 = interp1(np.array([xpt.cpu().numpy(), ypt.cpu().numpy()]).T)
 
-torch.cuda.empty_cache()
+# implementation
+interp2 = my_rgi((x, y), F)
+G2 = interp2(xpt,ypt)
 
-# our implementation
-interp_spline = my_rgi((x, y), F)
-G2 = interp_spline(xpt,ypt)
-
-fig, ax = plt.subplots(nrows=1, ncols=3, figsize=(12,3))
+fig, ax = plt.subplots(nrows=1, ncols=3, figsize=(18,6))
 
 vmin,vmax = (fun(np.concatenate([F.cpu().numpy().flatten(),G1])) for fun in (np.min,np.max))
 

pytorch_interp/interp.py
@@ -1,7 +1,6 @@
 
-import torch
 from pytorch_interp import bilinear_interp
-
+import torch
 
 class RegularGridInterpolator:
 
@@ -11,21 +10,21 @@ class RegularGridInterpolator:
         (x,y) = points
         self.x = x
         self.y = y
-        
-        assert isinstance(F, torch.Tensor)
-        self.F = F
-        self.G = torch.zeros(2**10)
-
         self.dx = float(x[1]-x[0])
         self.dy = float(y[1]-y[0])
         self.M1 = len(x)
         self.M2 = len(y)
+        self.F = F
+
 
     def __call__(self, xpt, ypt):
-        return bilinear_interp(
-            self.F, 
+        G = torch.zeros_like(xpt)
+        # G = torch.empty(xpt.size(), dtype=torch.float32)
+        bilinear_interp(
+            self.F, G,
             self.x, self.y,
             xpt, ypt,
             self.M1, self.M2,
             self.dx, self.dy,
         )
+        return G

pytorch_interp/ops.py
@@ -4,11 +4,11 @@ from torch import Tensor
 __all__ = ["bilinear_interp"]
 
 def bilinear_interp(
-                    F: Tensor, 
+                    F: Tensor, G: Tensor, 
                     x: Tensor, y: Tensor,
                     xpt: Tensor, ypt: Tensor,
                     M1: int, M2: int,
                     dx: float, dy: float
                     ) -> Tensor:
     """Bilinear interpolation"""
-    return torch.ops.extension_interp.bilinear_interp.default(F,x,y,xpt,ypt,M1,M2,dx,dy)
+    return torch.ops.extension_interp.bilinear_interp.default(F,G,x,y,xpt,ypt,M1,M2,dx,dy)